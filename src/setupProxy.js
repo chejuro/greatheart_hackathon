53553ccf8b4b0705fa539641e3608ce34ab74ec5
Asgar Zagitov <ihateyouthor@gmail.com>
2020-11-10 19:35:53 +0300

    AA

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,4 +3,19 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
     app.use(createProxyMiddleware('/requests', {target : 'http://80.87.197.233:8080'}));
     app.use(createProxyMiddleware('/entities', {target : 'http://80.87.197.233:8110'}));
+    app.use(createProxyMiddleware('/card', {target : 'http://80.87.197.233:8080', changeOrigin: true, pathRewrite: function (path, req) {
+    return req.originalUrl.replace('/card', '')    
+    // if (req.originalUrl.includes('/card/requests/create')) {
+    //         return 'http://80.87.197.233:8080/requests/create';
+
+    //     } 
+        
+    //     if (req.originalUrl.includes('/card/requests/change')) {
+    //         return 'http://80.87.197.233:8080/requests/change';
+
+    //     }
+        
+    //                 return 'http://80.87.197.233:8080/requests?requestId=';
+        
+    }}))
 };
