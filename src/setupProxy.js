16cde480abcd2287095a0d9d9d99da67d2c0e0e8
Asgar Zagitov <ihateyouthor@gmail.com>
2021-03-22 23:03:23 +0300

    fixes address

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-    app.use(createProxyMiddleware('/requests', {target : 'http://localhost:8080'}));
+    app.use(createProxyMiddleware('/requests', {target : 'http://80.87.197.233:8080'}));
     app.use(createProxyMiddleware('/blockchain', {target : 'http://79.174.13.214:8080'}));
     app.use(createProxyMiddleware('/entities', {target : 'http://80.87.197.233:8110'}));
     app.use(createProxyMiddleware('/enums', {target : 'http://80.87.197.233:8110'}));
